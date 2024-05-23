0e856b38de80ad08622b335523cfb01c07088049
changang-4life <163348160+changang-4life@users.noreply.github.com>
2024-05-24 09:27:56 +1200

    Update quiz_program_v8.py

--- quiz_program_v8.py
+++ quiz_program_v8.py
@@ -7,7 +7,7 @@ v8 ABCD check and list
 quizA_ques = ["1. What is the capital of New Zealand?\n", "2. Which city is known as 'The Garden City'?\n", "3. Where did L&P soda originally come from?\n", "4. In what month is Matariki celebrated?\n", "5. What colour is kakariki?\n"]
     # List for each question in Quiz A. Each question will be printed out when needed with its corresponding index number.
 
-quizB_ques = ["1. What is the anme of the stretch of water that seperates the North and South Islands?\n", "2. Which New Zealand city houses the Beehive?\n", "3. Which town has a giant carrot as a landmark?\n", "4. Where is 90 mile beach?\n", "5. When was the Treaty of Waitangi signed?\n"]
+quizB_ques = ["1. What is the name of the stretch of water that seperates the North and South Islands?\n", "2. Which New Zealand city houses the Beehive?\n", "3. Which town has a giant carrot as a landmark?\n", "4. Where is 90-mile beach?\n", "5. When was the Treaty of Waitangi signed?\n"]
     # List for each question in Quiz B. Each question will be printed out when needed with its corresponding index number.
 
 quizA_answers = ['B', 'B', 'C', 'C', 'A'] # Quiz answers placed in a list for ease of editing
@@ -25,7 +25,7 @@ quizA_ques3_op = ["A) Putaruru", "B) Waihi", "C) Paeroa", "D) Auckland\n"]
 quizA_ques4_op = ["A) April", "B) May", "C) May, June, July", "D) July\n"]
 quizA_ques5_op = ["A) Green", "B) Blue", "C) Black", "D) Grey\n"]
 
-quizB_ques1_op = ["A) Wellington Strait", "B) Tasman Chennel", "C) Cook Strait", "D) Kaikoura Strait"]
+quizB_ques1_op = ["A) Wellington Strait", "B) Tasman Channel", "C) Cook Strait", "D) Kaikoura Strait"]
 quizB_ques2_op = ["A) Wellington", "B) Christchurch", "C) Paeroa", "D) Auckland"]
 quizB_ques3_op = ["A) Taihape", "B) Waihi", "C) Paeroa", "D) Ohakune"]
 quizB_ques4_op = ["A) Top of the North Island", "B) Bottom of the South Island", "C) Bottom of the North Island", "D) Top of the South Island"]
@@ -187,6 +187,7 @@ def quizA():
 
 
 def quizB(): # For Quiz B
+    score = 0
 
     print(quizB_ques[0]) # Question 1 is called
     for options in quizB_ques1_op:
@@ -210,7 +211,7 @@ def quizB(): # For Quiz B
         score += 1
 
     elif user_answer != quizB_answers[0]: # However, if the user's answer does NOT match the first answer of this quiz's answer list...
-        print("Sorry, that was wrong)")
+        print("Sorry, that was wrong.")
 
     # . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 
